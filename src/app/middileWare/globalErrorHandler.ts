b0dabd88f79fbbd83f9b644ba876ce8ac988c749
sumonpaul55 <sumpaul100@gmail.com>
2024-08-15 17:56:46 +0600

    error handle duplicate error cast error and app error

--- src/app/middileWare/globalErrorHandler.ts
+++ src/app/middileWare/globalErrorHandler.ts
@@ -4,6 +4,9 @@ import handleZodError from "../erros/handleZodError";
 import { TErrorSources } from "../interface/error";
 import validateRequest from "./validateRequest";
 import handleValidationError from "../erros/handleValidationError";
+import handleCastError from "../erros/handleCastError";
+import handleDuplicateError from "../erros/handleDuplicateError";
+import AppError from "../erros/AppError";
 
 const globalErrorhandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode ? err.statusCode : 500;
@@ -24,8 +27,34 @@ const globalErrorhandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simpliFiedError.statusCode;
     message = simpliFiedError.message;
     errorSource = simpliFiedError.errorSource;
+  } else if (err?.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSource;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.erroSource;
+  } else if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+    errorSource = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorSource = [
+      {
+        path: "",
+        message: err.message,
+      },
+    ];
   }
-
   return res.status(statusCode).json({
     success: false,
     message,
