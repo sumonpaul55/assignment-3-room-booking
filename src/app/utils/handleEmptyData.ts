89f0cc224ff70df555fb2770a9c753d3188d77a0
sumonpaul55 <sumpaul100@gmail.com>
2024-08-16 17:43:47 +0600

    handle empty function turnery applied for prevent error

--- src/app/utils/handleEmptyData.ts
+++ src/app/utils/handleEmptyData.ts
@@ -2,7 +2,7 @@ import httpStatus from "http-status";
 import AppError from "../erros/AppError";
 
 const handleEmptyData = (data: [] | any) => {
-  if (data.length < 1) {
+  if (data?.length < 1) {
     throw new AppError(httpStatus.NOT_FOUND, "No Data Found");
   }
   return data;
